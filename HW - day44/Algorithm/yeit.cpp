404cc88f1199c1b5693916a0e97b03861d1ea3f8
yeit-ziral <qwg1447@gmail.com>
2023-04-25 01:31:14 +0900

    Update yeit.cpp

--- HW - day44/Algorithm/yeit.cpp
+++ HW - day44/Algorithm/yeit.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <queue>
 #include <algorithm>
 #include <iostream>
 
@@ -29,6 +30,63 @@ void dfs(string cur, vector<vector<string>> tickets)
                 visited[i] == true;
             }
         }
+}
+
+const int INF = 987654321;
+
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, 1, -1 };
+
+int solution(vector<vector<int>> maps)
+{
+    int n = maps.size();
+    int m = maps[0].size();
+    
+    vector<vector<int>> dist(n, vector<int>(m, INF));
+    vector<vector<bool>> visited(n, vector<bool>(m, false));
+
+    dist[0][0] = 1;
+    visited[0][0] = true;
+
+    queue<pair<int, int>> q;
+    q.push(make_pair(0, 0));
+
+    while (!q.empty())
+    {
+        int x = q.front().first;
+        int y = q.front().second;
+
+        q.pop();
+
+        for (int i = i < 4; i++)
+        {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+                continue;
+
+            if (maps[nx][ny] == 0)
+                continue;
+
+            if (!visited[nx][ny])
+            {
+                visited[nx][ny] == true;
+                dist[nx][ny] = dist[x][y] + 1;
+                q.push(make_pair(nx, ny));
+            }
+        }
+    }
+
+    vector<vector<bool>> discovered;
+    vector<vector<int>> 
+
+    int answer = dist[n - 1][m - 1];
+
+    if (answer == INF)
+        answer = -1;
+
+    return answer;
 }
 
 vector<string> solution(vector<vector<string>> tickets) {
