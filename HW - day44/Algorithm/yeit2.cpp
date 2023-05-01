1bd425e75d8bf571ae8cde610c6199302b1a7d8d
yeit-ziral <qwg1447@gmail.com>
2023-05-01 17:57:28 +0900

    s

--- HW - day44/Algorithm/yeit2.cpp
+++ HW - day44/Algorithm/yeit2.cpp
@@ -108,144 +108,148 @@
 //	return answer;
 //}
 
-#include <string>
+#include <iostream>
 #include <vector>
 #include <list>
-#include <algorithm>
 #include <map>
-#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <string>
 
 using namespace std;
 
-void Sapperate(string letter, vector<string> Letter)
-{
-	map<int, string> A;
+//void Sapperate(string letter, vector<string> Letter)
+//{
+//	map<int, string> A;
+//
+//	while (true)
+//	{
+//		string a;
+//
+//		string s;
+//
+//		for (int i = 0; i < letter.size(); i++)
+//		{
+//			
+//
+//			if (s == " ")
+//				break;
+//
+//			a += s;
+//		}
+//
+//		Letter.push_back(a);
+//	}
+//
+//
+//	/*while (true)
+//	{
+//		if (letter.empty())
+//			break;
+//		
+//		Letter;
+//
+//		string le;
+//
+//		for (int i = 0; i < letter.size(); i++)
+//		{
+//			while (letter[i] != ' ')
+//			{
+//				string s(1, letter[i]);
+//				
+//				le += s;
+//
+//				letter.erase(0, i + 1);
+//			}
+//		}
+//
+//		Letter.push_back(le);
+//
+//		Sapperate(letter, Letter);
+//	}*/
+//}
+//
+//string solution(string letter) {
+//	string answer = "";
+//
+//	vector<string> Letter;
+//
+//	std::string delimiter = " ";
+//	size_t pos = 0;
+//	std::string token;
+//	while ((pos = letter.find(delimiter)) != std::string::npos) {
+//		token = letter.substr(0, pos);
+//		Letter.push_back(token);
+//		letter.erase(0, pos + delimiter.length());
+//	}
+//	Letter.push_back(letter);
+//
+//	map<string, string> morse =
+//	{
+//		{"a", ".-"}, {"b", "-..."}, {"c" , "-.-."}, {"d" , "-.."}, {"e" , "."}, {"f" , "..-."},
+//		{"g" , "--."}, {"h" , "...."}, {"i" , ".."}, {"j" , ".---"}, {"k" , "-.-"}, {"l" , ".-.."},
+//		{"m" , "--"}, {"n" , "-."}, {"o" , "---"},{"p" , ".--."}, {"q" , "--.-"}, {"r" , ".-."},
+//		{"s" , "..."}, {"t" , "-"}, {"u", "..-"}, {"v" , "...-"}, {"w" , ".--"}, {"x" , "-..-"},
+//		{"y" , "-.--"}, {"z" , "--.."}
+//	};
+//
+//	for (int i = 0; i < Letter.size(); i++)
+//	{
+//		for (auto iter : morse) {
+//			if (iter.second == Letter[i])
+//			{
+//				answer += iter.first;
+//			}
+//		}
+//	}
+//
+//	return answer;
+//}
+//
+//using namespace std;
+//
+//int solution(string before, string after) {
+//	int answer = 0;
+//	vector<char> a;
+//	vector<char> b;
+//	for (auto s : before)
+//	{
+//		b.push_back(s);
+//	}
+//
+//	for (auto s : after)
+//	{
+//		a.push_back(s);
+//	}
+//
+//	if (before.size() != after.size())
+//		answer = 1;
+//	else
+//	{
+//		for (int i = 0; i < b.size(); i++)
+//		{
+//			for (int j = 0; j < a.size(); j++)
+//			{
+//				if (b[i] == a[j])
+//				{
+//					b.erase(b[i], b[i]);
+//				}
+//			}
+//		}
+//	}
+//
+//	return answer;
+//}
+//
+//int main()
+//{
+//
+//	string Letter1 = ".... . .-.. .-.. ---";
+//
+//	std::cout << solution(Letter1) << std::endl;
+//
+//	return 0;
+//}
 
-	while (true)
-	{
-		string a;
-
-		string s;
-
-		for (int i = 0; i < letter.size(); i++)
-		{
-			
-
-			if (s == " ")
-				break;
-
-			a += s;
-		}
-
-		Letter.push_back(a);
-	}
-
-
-	/*while (true)
-	{
-		if (letter.empty())
-			break;
-		
-		Letter;
-
-		string le;
-
-		for (int i = 0; i < letter.size(); i++)
-		{
-			while (letter[i] != ' ')
-			{
-				string s(1, letter[i]);
-				
-				le += s;
-
-				letter.erase(0, i + 1);
-			}
-		}
-
-		Letter.push_back(le);
-
-		Sapperate(letter, Letter);
-	}*/
-}
-
-string solution(string letter) {
-	string answer = "";
-
-	vector<string> Letter;
-
-	std::string delimiter = " ";
-	size_t pos = 0;
-	std::string token;
-	while ((pos = letter.find(delimiter)) != std::string::npos) {
-		token = letter.substr(0, pos);
-		Letter.push_back(token);
-		letter.erase(0, pos + delimiter.length());
-	}
-	Letter.push_back(letter);
-
-	map<string, string> morse =
-	{
-		{"a", ".-"}, {"b", "-..."}, {"c" , "-.-."}, {"d" , "-.."}, {"e" , "."}, {"f" , "..-."},
-		{"g" , "--."}, {"h" , "...."}, {"i" , ".."}, {"j" , ".---"}, {"k" , "-.-"}, {"l" , ".-.."},
-		{"m" , "--"}, {"n" , "-."}, {"o" , "---"},{"p" , ".--."}, {"q" , "--.-"}, {"r" , ".-."},
-		{"s" , "..."}, {"t" , "-"}, {"u", "..-"}, {"v" , "...-"}, {"w" , ".--"}, {"x" , "-..-"},
-		{"y" , "-.--"}, {"z" , "--.."}
-	};
-
-	for (int i = 0; i < Letter.size(); i++)
-	{
-		for (auto iter : morse) {
-			if (iter.second == Letter[i])
-			{
-				answer += iter.first;
-			}
-		}
-	}
-
-	return answer;
-}
-
-using namespace std;
-
-int solution(string before, string after) {
-	int answer = 0;
-	vector<char> a;
-	vector<char> b;
-	for (auto s : before)
-	{
-		b.push_back(s);
-	}
-
-	for (auto s : after)
-	{
-		a.push_back(s);
-	}
-
-	if (before.size() != after.size())
-		answer = 1;
-	else
-	{
-		for (int i = 0; i < b.size(); i++)
-		{
-			for (int j = 0; j < a.size(); j++)
-			{
-				if (b[i] == a[j])
-				{
-					b.erase(b[i], b[i]);
-				}
-			}
-		}
-	}
-
-	return answer;
-}
-
-int main()
-{
-
-	string Letter1 = ".... . .-.. .-.. ---";
-
-	std::cout << solution(Letter1) << std::endl;
-
-	return 0;
-}
