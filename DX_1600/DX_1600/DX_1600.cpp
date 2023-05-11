3f778ae2a91b306d6fafcc45c7e7fd3a60c76f17
yeit-ziral <qwg1447@gmail.com>
2023-05-11 15:53:09 +0900

    Update DX_1600.cpp

--- DX_1600/DX_1600/DX_1600.cpp
+++ DX_1600/DX_1600/DX_1600.cpp
@@ -376,33 +376,33 @@ void InitDevice()
     Vertex temp;
     temp.pos = XMFLOAT3(-0.5f, 0.5f, 0.0f);
     temp.color = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
-    temp.uv = { 0.0f, 0.0f };
+    temp.uv = { 1.0f, 0.0f };
     vertices.push_back(temp); // 왼쪽 위
 
     temp.pos = XMFLOAT3(0.5f, 0.5f, 0.0f);
     temp.color = XMFLOAT4(0.7f, 0.6f, 1.0f, 1.0f);
-    temp.uv = { 1.0f, 0.0f };
+    temp.uv = { 0.0f, 0.0f };
     vertices.push_back(temp); // 오른쪽 위
 
     temp.pos = XMFLOAT3(0.5f, -0.5f, 0.0f);
     temp.color = XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f);
-    temp.uv = { 1.0f, 1.0f };
+    temp.uv = { 0.0f, 1.0f };
     vertices.push_back(temp); // 오른쪽 아래
 
     // 사각형 반쪽 삼각형
     temp.pos = XMFLOAT3(-0.5f, 0.5f, 0.0f);
     temp.color = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
-    temp.uv = { 0.0f, 0.0f };
+    temp.uv = { 1.0f, 0.0f };
     vertices.push_back(temp); // 왼쪽 위
 
     temp.pos = XMFLOAT3(0.5f, -0.5f, 0.0f);
     temp.color = XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f);
-    temp.uv = { 1.0f, 1.0f };
+    temp.uv = { 0.0f, 1.0f };
     vertices.push_back(temp); // 오른쪽 아래
 
     temp.pos = XMFLOAT3(-0.5f, -0.5f, 0.0f);
     temp.color = XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f);
-    temp.uv = { 0.0f, 1.0f };
+    temp.uv = { 1.0f, 1.0f };
     vertices.push_back(temp); // 왼쪽 아래
 
     D3D11_BUFFER_DESC bd = {};
