d3d8f30dc1cc42922f18a9b6a74fc23de1d59517
yeit-ziral <qwg1447@gmail.com>
2023-08-31 01:58:22 +0900

    agf

---
+++ 새 폴더/DX_1600/DX_1600/Object/CupHead/Items/SpeedBoots.cpp
@@ -0,0 +1,18 @@
+#include "framework.h"
+#include "SpeedBoots.h"
+
+SpeedBoots::SpeedBoots()
+{
+}
+
+SpeedBoots::~SpeedBoots()
+{
+}
+
+void SpeedBoots::Update()
+{
+}
+
+void SpeedBoots::Render()
+{
+}
