bdc29acde6bb2a60bc2dffe2b3fb5b6817fa4108
BangJiho <qwg1447@gmail.com>
2023-09-01 12:19:00 +0900

    inven 제작중

--- 새 폴더/DX_1600/DX_1600/Object/UI/inventory.cpp
+++ 새 폴더/DX_1600/DX_1600/Object/UI/inventory.cpp
@@ -1,2 +1,56 @@
 #include "framework.h"
+#include "InventoryCell.h"
 #include "inventory.h"
+
+inventory::inventory()
+{
+	_invenCell1 = make_shared<InventoryCell>();
+	_invenCell2 = make_shared<InventoryCell>();
+	_invenCell3 = make_shared<InventoryCell>();
+	_invenCell4 = make_shared<InventoryCell>();
+}
+
+inventory::~inventory()
+{
+}
+
+void inventory::Update()
+{
+}
+
+void inventory::PostRender()
+{
+}
+
+void inventory::FillInventory(InventoryCell::Items item)
+{
+	if (_invenCell1->_isFilled = false)
+	{
+		_invenCell1->SetItemState(item);
+		_invenCell1->_isFilled = true;
+	}
+	if (_invenCell2->_isFilled = false)
+	{
+		_invenCell2->SetItemState(item);
+		_invenCell2->_isFilled = true;
+	}
+	if (_invenCell3->_isFilled = false)
+	{
+		_invenCell3->SetItemState(item);
+		_invenCell3->_isFilled = true;
+	}
+	if (_invenCell4->_isFilled = false)
+	{
+		_invenCell4->SetItemState(item);
+		_invenCell4->_isFilled = true;
+	}
+}
+
+void inventory::DropItems()
+{
+}
+
+InventoryCell::Items inventory::GetInvenState()
+{
+	return _invenCell1->GetItemState();
+}
--- 새 폴더/DX_1600/DX_1600/Object/UI/inventory.h
+++ 새 폴더/DX_1600/DX_1600/Object/UI/inventory.h
@@ -6,6 +6,18 @@ public:
 	inventory();
 	~inventory();
 
+	void Update();
+	void PostRender();
+
+	// 인벤 채우는 함수, 1채우기 시작해서 해당 _invenCell의 _isFilled = true이면 다음 _invenCell으로 이동
+	void FillInventory(InventoryCell::Items item);
+
+	// 인벤 지우는 함수, 맨 앞 cell부터 버림, 바로 뒤 cell의 State를 가져오고 맨 뒤의 cell은 None으로 만들기
+	void DropItems();
+
+	// _invenCell1의 Items 를 넘겨주는 함수
+	InventoryCell::Items GetInvenState();
+
 private:
 	//아이템 먹을 시 순서대로 각 이벤토리 칸에 해당 아이템의 정보 불러오기 
 	shared_ptr<class InventoryCell> _invenCell1;
